e488d72406ad35829cd7df3e110090049f211026
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-22 17:50:42 -0500

    add first and last name to object and update code

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 
 //Practicing map, filter, reduce, find, forEach
 const employees = [
-  { name: 'Alice', age: 28, occupation: 'Engineer' },
-  { name: 'Bob', age: 34, occupation: 'Designer' },
-  { name: 'Charlie', age: 22, occupation: 'Student' },
-  { name: 'Diana', age: 45, occupation: 'Manager' },
-  { name: 'Ethan', age: 30, occupation: 'Developer' },
-  { name: 'Ryan', age: 46, occupation: 'Developer' },
+    { firstName: 'Alice', lastName: 'Smith', age: 28, occupation: 'Engineer' },
+    { firstName: 'Bob', lastName: 'Johnson', age: 34, occupation: 'Designer' },
+    { firstName: 'Charlie', lastName: 'Brown', age: 22, occupation: 'Student' },
+    { firstName: 'Diana', lastName: 'Davis', age: 45, occupation: 'Manager' },
+    { firstName: 'Ethan', lastName: 'Miller', age: 30, occupation: 'Developer' },
+    { firstName: 'Ryan', lastName: 'Wilson', age: 46, occupation: 'Developer' },
 ];
 
-const names = employees.map((person) => person.name);
-console.log(names);
+const names = employees.map((person) => person.firstName);
+console.log(employees);
 
 const junior = employees.filter((person) => person.age < 30);
 
 const midLevel = employees.filter(
   (person) => person.age > 30 && person.age < 40,
 );
-// console.log(midLevel);
+console.log(midLevel);
+
+const senior = employees.filter((person) => person.age > 40);
+console.log(senior);
 
 // const nameRole = employees.map(person => `${person.name} is a ${person.occupation}`);
 // console.log(nameRole);
@@ -28,7 +31,7 @@ const student = employees.find((person) => person.occupation === 'Student');
 console.log(student);
 
 const nameToAgeMap = employees.reduce((obj, employee) => {
-  obj[employee.name] = employee.age;
+  obj[employee.firstName] = employee.age;
   return obj;
 }, {});
 
