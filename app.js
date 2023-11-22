310d8452bcd4a2e4ae8e51374fe099a29f9bc6e2
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-22 18:10:25 -0500

    figured i would pump the data to the screen two reasons. 1. to see what I am doing 2. to see if I can get the data to the screen it works

--- app.js
+++ app.js
@@ -1,39 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const displayElement = document.getElementById('dataDisplay');
 
-//Practicing map, filter, reduce, find, forEach
-const employees = [
+  const employees = [
     { firstName: 'Alice', lastName: 'Smith', age: 28, occupation: 'Engineer' },
     { firstName: 'Bob', lastName: 'Johnson', age: 34, occupation: 'Designer' },
     { firstName: 'Charlie', lastName: 'Brown', age: 22, occupation: 'Student' },
     { firstName: 'Diana', lastName: 'Davis', age: 45, occupation: 'Manager' },
-    { firstName: 'Ethan', lastName: 'Miller', age: 30, occupation: 'Developer' },
+    {
+      firstName: 'Ethan',
+      lastName: 'Miller',
+      age: 30,
+      occupation: 'Developer',
+    },
     { firstName: 'Ryan', lastName: 'Wilson', age: 46, occupation: 'Developer' },
-];
+  ];
 
-const names = employees.map((person) => person.firstName);
-console.log(employees);
+  const junior = employees.filter((person) => person.age < 30);
+  const midLevel = employees.filter(
+    (person) => person.age > 30 && person.age < 40,
+  );
+  const senior = employees.filter((person) => person.age > 40);
+  const student = employees.find((person) => person.occupation === 'Student');
+  const nameToAgeMap = employees.reduce((obj, employee) => {
+    obj[employee.firstName] = employee.age;
+    return obj;
+  }, {});
 
-const junior = employees.filter((person) => person.age < 30);
+  displayElement.innerHTML = '<h2>All Employees</h2>';
+  displayElement.innerHTML +=
+    '<pre>' + JSON.stringify(employees, null, 2) + '</pre>';
 
-const midLevel = employees.filter(
-  (person) => person.age > 30 && person.age < 40,
-);
-console.log(midLevel);
+  displayElement.innerHTML += '<h2>Junior Employees</h2>';
+  displayElement.innerHTML +=
+    '<pre>' + JSON.stringify(junior, null, 2) + '</pre>';
 
-const senior = employees.filter((person) => person.age > 40);
-console.log(senior);
+  displayElement.innerHTML += '<h2>Mid-Level Employees</h2>';
+  displayElement.innerHTML +=
+    '<pre>' + JSON.stringify(midLevel, null, 2) + '</pre>';
 
-// const nameRole = employees.map(person => `${person.name} is a ${person.occupation}`);
-// console.log(nameRole);
+  displayElement.innerHTML += '<h2>Senior Employees</h2>';
+  displayElement.innerHTML +=
+    '<pre>' + JSON.stringify(senior, null, 2) + '</pre>';
 
-// employees.forEach(person => console.log(`${person.name} is a ${person.occupation} at our company`))
+  displayElement.innerHTML += '<h2>Student</h2>';
+  displayElement.innerHTML +=
+    '<pre>' + JSON.stringify(student, null, 2) + '</pre>';
 
-const student = employees.find((person) => person.occupation === 'Student');
-console.log(student);
-
-const nameToAgeMap = employees.reduce((obj, employee) => {
-  obj[employee.firstName] = employee.age;
-  return obj;
-}, {});
-
-console.log(nameToAgeMap);
-// { Alice: 28, Bob: 34, Charlie: 22, Diana: 45, Ethan: 30 }
+  displayElement.innerHTML += '<h2>Name to Age Map</h2>';
+  displayElement.innerHTML +=
+    '<pre>' + JSON.stringify(nameToAgeMap, null, 2) + '</pre>';
+});
